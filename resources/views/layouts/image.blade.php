b1a6e82fe133d993ac88b27cbefeedd19cace568
unknown <chadiegilaugis@gmail.com>
2022-11-13 09:37:09 +0800

    job seeker

--- resources/views/layouts/image.blade.php
+++ resources/views/layouts/image.blade.php
@@ -1,2 +1,4 @@
-<div class="container-fluid d-flex justify-content-center align-items-center mb-2"><a class="navbar-brand" href="#" style="font-size: 55px;font-family: bodoni mt;"><img src="{{ URL::asset('images/blogo.jpg') }}" style="resize: both;width: 100%;"></a>
-        </div>
+<div class="container-fluid d-flex justify-content-center align-items-center mb-2"><a class="navbar-brand" href="#"
+        style="font-size: 55px;font-family: bodoni mt;"><img src="{{ URL::asset('images/blogo.jpg') }}"
+            style="resize: both;width: 100%;"></a>
+</div>
