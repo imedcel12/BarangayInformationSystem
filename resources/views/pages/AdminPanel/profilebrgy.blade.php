20c5f74057e8d98c18d7931c67e11ddc9100e071
BrenieEmnace <emnacebrenie@gmail.com>
2022-11-22 23:44:56 +0800

    changes

--- resources/views/pages/AdminPanel/profilebrgy.blade.php
+++ resources/views/pages/AdminPanel/profilebrgy.blade.php
@@ -21,15 +21,14 @@
             <div class="card-body">
                 <h5 class="card-title text-center">Mission</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
+                <p class="card-text">To deliver efficient quality and productive service to the constituents.</p>
                 <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>
             </div>
         </div>
         <div class="card" style="width: 26rem;">
             <div class="card-body">
-                <h5 class="card-title text-center">Vission</h5>
+                <h5 class="card-title text-center">Vision</h5>
                 <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
                     content.</p>
